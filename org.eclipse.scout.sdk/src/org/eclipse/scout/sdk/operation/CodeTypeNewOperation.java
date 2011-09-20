520d7500054fd430b527d8d893fec5107980a4fd
Andreas Hoegger <Andreas.Hoegger@bsiag.com>
2011-09-20 10:06:28 +0000

    updated source format op.

--- org.eclipse.scout.sdk/src/org/eclipse/scout/sdk/operation/CodeTypeNewOperation.java
+++ org.eclipse.scout.sdk/src/org/eclipse/scout/sdk/operation/CodeTypeNewOperation.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -41,6 +41,7 @@ public class CodeTypeNewOperation implements IOperation {
   private IType m_createdType;
   private boolean m_formatSource;
 
+  @Override
   public String getOperationName() {
     return "New Code Type...";
   }
@@ -55,6 +56,7 @@ public class CodeTypeNewOperation implements IOperation {
     }
   }
 
+  @Override
   public void run(IProgressMonitor monitor, IScoutWorkingCopyManager workingCopyManager) throws CoreException {
     ScoutTypeNewOperation newOp = new ScoutTypeNewOperation(getTypeName(), getSharedBundle().getPackageName(IScoutBundle.SHARED_PACKAGE_APPENDIX_SERVICES_CODE), getSharedBundle());
     newOp.setSuperTypeSignature(getSuperTypeSignature());
