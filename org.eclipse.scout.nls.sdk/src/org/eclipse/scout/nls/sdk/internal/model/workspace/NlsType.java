6f8bd325568c7e76c9978abfcb4fd076b6423d77
Andreas Hoegger <Andreas.Hoegger@bsiag.com>
2011-10-17 21:53:55 +0000

    Incomplete - task : RAP SDK Support

--- org.eclipse.scout.nls.sdk/src/org/eclipse/scout/nls/sdk/internal/model/workspace/NlsType.java
+++ org.eclipse.scout.nls.sdk/src/org/eclipse/scout/nls/sdk/internal/model/workspace/NlsType.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *     BSI Business Systems Integration AG - initial API and implementation
  ******************************************************************************/
@@ -68,7 +68,7 @@ public class NlsType implements INlsType {
   public void reload() {
     try {
       IField field = m_type.getField(INlsConstants.RESOURCE_BUNDLE_FIELD_NAME);
-      if (field != null && field.exists() ) {
+      if (field != null && field.exists()) {
         int flags = field.getFlags();
         int refFlags = Flags.AccPublic | Flags.AccStatic;
         if ((refFlags & flags) == refFlags) {
@@ -91,7 +91,7 @@ public class NlsType implements INlsType {
       m_superTypes = typeHierarchy.getAllSuperclasses(m_type);
 //      IType superclass = typeHierarchy.getSuperclass(m_type);
       // String superClassPluginId = PluginRegistry.findModel(superclass.getJavaProject().getProject()).getPluginBase().getId();
-      if (m_superTypes.length> 0 && m_superTypes[0].exists() &&  !m_superTypes[0].getFullyQualifiedName().equals(DynamicNls.class.getName())) {
+      if (m_superTypes.length > 0 && m_superTypes[0].exists() && !m_superTypes[0].getFullyQualifiedName().equals(DynamicNls.class.getName())) {
         m_propertySupport.setProperty(PROP_SUPER_TYPE, m_superTypes[0]);
       }
     }
@@ -100,6 +100,7 @@ public class NlsType implements INlsType {
     }
   }
 
+  @Override
   public IType getType() {
     return m_type;
   }
@@ -108,6 +109,7 @@ public class NlsType implements INlsType {
     m_propertySupport.setPropertyString(PROP_TRANSLATION_FOLDER_NAME, name);
   }
 
+  @Override
   public String getTranslationsFolderName() {
     return m_propertySupport.getPropertyString(PROP_TRANSLATION_FOLDER_NAME);
   }
@@ -116,6 +118,7 @@ public class NlsType implements INlsType {
     m_propertySupport.setPropertyString(PROP_TRANSLATION_FILE_PREFIX, filePrefix);
   }
 
+  @Override
   public String getTranslationsPrefix() {
     return m_propertySupport.getPropertyString(PROP_TRANSLATION_FILE_PREFIX);
   }
@@ -124,13 +127,14 @@ public class NlsType implements INlsType {
     return (IType) m_propertySupport.getProperty(PROP_SUPER_TYPE);
   }
 
-  public IType[] getAllSuperclasses(){
+  @Override
+  public IType[] getAllSuperclasses() {
     return m_superTypes;
   }
 
   /**
    * commodity
-   *
+   * 
    * @return
    */
   public IJavaProject getJavaProject() {
@@ -146,13 +150,15 @@ public class NlsType implements INlsType {
   }
 
   private class P_NlsResourceChangeListener implements IResourceChangeListener {
+    @Override
     public void resourceChanged(IResourceChangeEvent event) {
       IResourceDelta delta = event.getDelta();
       try {
         if (delta != null) {
           delta.accept(new IResourceDeltaVisitor() {
-            public boolean visit(IResourceDelta delta) {
-              IResource resource = delta.getResource();
+            @Override
+            public boolean visit(IResourceDelta resDelta) {
+              IResource resource = resDelta.getResource();
               if (resource != null && resource.equals(m_type.getResource())) {
                 if (m_type.getResource().exists()) {
                   reload();
