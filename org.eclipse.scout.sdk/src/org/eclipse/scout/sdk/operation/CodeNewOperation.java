919fc83bba37c894250ccc1cc52eb5476bbea3e3
Matthias Villiger <mvi@bsiag.com>
2012-01-06 13:41:44 +0000

    https://bugs.eclipse.org/bugs/show_bug.cgi?id=364891: "New Code type editor does not accept String value" step four: automatically migrate legacy nested codes to new static style (ensures no compilation errors)

--- org.eclipse.scout.sdk/src/org/eclipse/scout/sdk/operation/CodeNewOperation.java
+++ org.eclipse.scout.sdk/src/org/eclipse/scout/sdk/operation/CodeNewOperation.java
@@ -10,13 +10,23 @@
  ******************************************************************************/
 package org.eclipse.scout.sdk.operation;
 
+import java.util.List;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jdt.core.Flags;
+import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.Signature;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.Modifier;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.Document;
 import org.eclipse.scout.commons.StringUtility;
 import org.eclipse.scout.nls.sdk.model.INlsEntry;
 import org.eclipse.scout.sdk.RuntimeClasses;
@@ -26,9 +36,12 @@ import org.eclipse.scout.sdk.operation.method.NlsTextMethodUpdateOperation;
 import org.eclipse.scout.sdk.operation.util.JavaElementFormatOperation;
 import org.eclipse.scout.sdk.operation.util.OrderedInnerTypeNewOperation;
 import org.eclipse.scout.sdk.util.ScoutUtility;
+import org.eclipse.scout.sdk.util.log.ScoutStatus;
 import org.eclipse.scout.sdk.util.signature.IImportValidator;
 import org.eclipse.scout.sdk.util.type.TypeUtility;
 import org.eclipse.scout.sdk.util.typecache.IWorkingCopyManager;
+import org.eclipse.text.edits.MalformedTreeException;
+import org.eclipse.text.edits.TextEdit;
 
 /**
  * <h3>CodeNewOperation</h3> ...
@@ -73,8 +86,64 @@ public class CodeNewOperation implements IOperation {
     }
   }
 
+  @SuppressWarnings("unchecked")
+  private static void setStaticModifierRec(TypeDeclaration td) {
+    List l = td.modifiers();
+    Modifier modStatic = td.getAST().newModifier(Modifier.ModifierKeyword.STATIC_KEYWORD);
+    boolean isStatic = false;
+    for (Object o : l) { // check if a static modifier is already present (contains does not work)
+      if (o instanceof Modifier) {
+        if (((Modifier) o).isStatic()) {
+          isStatic = true;
+          break;
+        }
+      }
+    }
+    if (!isStatic) {
+      td.modifiers().add(modStatic);
+    }
+    for (TypeDeclaration child : td.getTypes()) {
+      setStaticModifierRec(child);
+    }
+  }
+
+  private void setStaticModifierForAllCodes() throws CoreException {
+    ICompilationUnit cu = getDeclaringType().getCompilationUnit();
+    String source = cu.getSource();
+    Document document = new Document(source);
+    ASTParser parser = ASTParser.newParser(AST.JLS3);
+    parser.setSource(source.toCharArray());
+    CompilationUnit root = (CompilationUnit) parser.createAST(null);
+
+    root.recordModifications();
+    for (Object type : root.types()) {
+      if (type instanceof TypeDeclaration) {
+        for (TypeDeclaration c : ((TypeDeclaration) type).getTypes())
+          setStaticModifierRec(c);
+      }
+    }
+
+    TextEdit edits = root.rewrite(document, cu.getJavaProject().getOptions(true));
+    try {
+      edits.apply(document);
+    }
+    catch (MalformedTreeException e) {
+      throw new CoreException(new ScoutStatus(e));
+    }
+    catch (BadLocationException e) {
+      throw new CoreException(new ScoutStatus(e));
+    }
+    String newSource = document.get();
+    cu.getBuffer().setContents(newSource);
+  }
+
   @Override
   public void run(IProgressMonitor monitor, IWorkingCopyManager workingCopyManager) throws CoreException {
+    // to correct legacy codes: in older SDK releases nested Code classes in a CodeType have not been declared static.
+    // Go through all legacy codes in the current compilation unit and make them static -> old codes are migrated to the new static style.
+    // This is needed because if a new child Code is created inside an old (not static) Code, this leads to compilation errors.
+    setStaticModifierForAllCodes();
+
     OrderedInnerTypeNewOperation codeOp = new OrderedInnerTypeNewOperation(getTypeName(), getDeclaringType());
     codeOp.setFormatSource(false);
     codeOp.setSibling(getSibling());
