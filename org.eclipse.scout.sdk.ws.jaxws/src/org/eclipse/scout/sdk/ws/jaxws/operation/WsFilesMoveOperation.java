a4f464f66db67c27b91856b1113ca9e038d6f6ad
Daniel Wiehl <daniel.wiehl@bsiag.com>
2012-01-06 00:02:40 +0000

    06.01.2012 dwi Bugzilla: 367994 Problem: a) The WSDL file of webservice providers whose WSDL file is located in a sub-folder of '/WEB-INF/wsdl' is dynamically generated at JAX-WS bootstrap instead of the existing WSDL file to be published. b) Webservice consumer specific WSDL files might interfere with provider specific WSDL files, e.g. if defining same services or ports. In consequence, all webservice providers of that plugin are not published. c) redundant WSDL file in webservice stub JAR-file. Solution: a) changed JAX-WS resource loading to also look in subfolders for existing resources b) WS provider specific WSDL files should be located in the folder '/WEB-INF/wsdl/provider' whereas consumer specific WSDL files in '/WEB-INF/wsdl/consumer'. In turn, JAX-WS is instrumented to ignore all the files in the (sub-)folder '/WEB-INF/wsdl/consumer' when publishing the endpoints. c) WSDL file is not put into the JAR archive anymore when building the webservice stub. Migration: - Use Scout SDK (repair actions) to move WSDL files of webservice providers into folder '/WEB-INF/wsdl/provider' and of webservice consumer into folder '/WEB-INF/wsdl/consumers' - Rebuild webservice stubs (should automatically be done if using repair action)

--- org.eclipse.scout.sdk.ws.jaxws/src/org/eclipse/scout/sdk/ws/jaxws/operation/WsFilesMoveOperation.java
+++ org.eclipse.scout.sdk.ws.jaxws/src/org/eclipse/scout/sdk/ws/jaxws/operation/WsFilesMoveOperation.java
@@ -130,6 +130,9 @@ public class WsFilesMoveOperation implements IOperation {
       return;
     }
     try {
+      // ensure folder to be created
+      JaxWsSdkUtility.getFolder(m_bundle, JaxWsSdkUtility.getParentFolder(m_bundle, file).getProjectRelativePath().toPortableString(), true);
+      // move file
       file.move(destinationFolderPath.getFullPath().append(file.getName()), true, true, monitor);
     }
     catch (Exception e) {
