ba46f3852aeabb8b98f325af9c12efcf23a15e76
Matthias Villiger <mvi@bsiag.com>
2012-01-25 10:53:51 +0000

    https://bugs.eclipse.org/bugs/show_bug.cgi?id=363041: "Eclipse Juno: Scout SDK" rap ui project creation fixed on eclilpse 3.5, ensure scout form data auto update is always started (startup extension)

--- org.eclipse.scout.sdk.rap.ui/src/org/eclipse/scout/sdk/rap/ui/internal/extensions/bundle/RapScoutBundleExtension.java
+++ org.eclipse.scout.sdk.rap.ui/src/org/eclipse/scout/sdk/rap/ui/internal/extensions/bundle/RapScoutBundleExtension.java
@@ -12,27 +12,37 @@ package org.eclipse.scout.sdk.rap.ui.internal.extensions.bundle;
 
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.scout.sdk.Texts;
+import org.eclipse.scout.sdk.compatibility.internal.PlatformVersionUtility;
 import org.eclipse.scout.sdk.rap.ui.internal.wizard.project.RapTargetPlatformWizardPage;
 import org.eclipse.scout.sdk.ui.extensions.bundle.IScoutBundleProvider;
 import org.eclipse.scout.sdk.ui.extensions.project.IScoutBundleExtension.BundleTypes;
 import org.eclipse.scout.sdk.ui.internal.ScoutSdkUi;
 import org.eclipse.scout.sdk.ui.wizard.AbstractScoutWizardPage;
 import org.eclipse.scout.sdk.ui.wizard.project.IScoutProjectWizard;
+import org.osgi.framework.Version;
 
 public class RapScoutBundleExtension implements IScoutBundleProvider {
+  private boolean m_rapUiSelected;
+
   public RapScoutBundleExtension() {
+    m_rapUiSelected = true;/* by default all nodes are checked */
   }
 
   @Override
   public IStatus getStatus(IScoutProjectWizard wizard) {
     if (!wizard.getProjectWizardPage().hasSelectedBundle(BundleTypes.Client_Bundle)) {
-      return new Status(IStatus.ERROR, ScoutSdkUi.PLUGIN_ID, "Be aware of creating a RAP bundle without a client bundle.");
+      return new Status(IStatus.ERROR, ScoutSdkUi.PLUGIN_ID, Texts.get("NoRapWithoutClient"));
+    }
+    if (m_rapUiSelected && PlatformVersionUtility.getPlatformVersion().compareTo(new Version(3, 6, 0)) < 0) {
+      return new Status(IStatus.ERROR, ScoutSdkUi.PLUGIN_ID, Texts.get("NoRapBeforeHelios"));
     }
     return Status.OK_STATUS;
   }
 
   @Override
   public void bundleSelectionChanged(IScoutProjectWizard wizard, boolean selected) {
+    m_rapUiSelected = selected;
     AbstractScoutWizardPage page = wizard.getPage(RapTargetPlatformWizardPage.class.getName());
     if (page == null) {
       page = new RapTargetPlatformWizardPage();
