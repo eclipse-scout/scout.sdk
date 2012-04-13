e13653b4513e4e9238d7b1e821fb18c8e8e010ae
Matthias Villiger <mvi@bsiag.com>
2012-04-13 08:44:18 +0000

    bugfix for SWT UI: desktop title is now used for the window title

--- org.eclipse.scout.sdk/templates/ui.swt/src/SwtEnvironment.java
+++ org.eclipse.scout.sdk/templates/ui.swt/src/SwtEnvironment.java
@@ -10,19 +10,26 @@
  ******************************************************************************/
 package @@BUNDLE_SWT_NAME@@;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
 import org.eclipse.scout.rt.client.AbstractClientSession;
+import org.eclipse.scout.rt.client.ui.desktop.IDesktop;
 import org.eclipse.scout.rt.client.ui.form.IForm;
 import org.eclipse.scout.rt.ui.swt.AbstractSwtEnvironment;
 import org.eclipse.scout.rt.ui.swt.ISwtEnvironmentListener;
 import org.eclipse.scout.rt.ui.swt.SwtEnvironmentEvent;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
+import org.osgi.framework.Bundle;
+
 import @@BUNDLE_SWT_NAME@@.editor.ScoutEditorPart;
 import @@BUNDLE_SWT_NAME@@.views.CenterView;
 import @@BUNDLE_SWT_NAME@@.views.DetailView;
 import @@BUNDLE_SWT_NAME@@.views.EastView;
 import @@BUNDLE_SWT_NAME@@.views.OutlineView;
 import @@BUNDLE_SWT_NAME@@.views.TableView;
-import org.osgi.framework.Bundle;
 
 public class SwtEnvironment extends AbstractSwtEnvironment{
 
@@ -44,5 +51,37 @@ public class SwtEnvironment extends AbstractSwtEnvironment{
         }
       }
     });
+    addEnvironmentListener(new ISwtEnvironmentListener() {
+      @Override
+      public void environmentChanged(SwtEnvironmentEvent e) {
+        if (e.getType() == SwtEnvironmentEvent.STARTED) {
+          removeEnvironmentListener(this);
+          IDesktop d = getClientSession().getDesktop();
+          if (d != null) {
+            setWindowTitle(d.getTitle());
+            d.addPropertyChangeListener(IDesktop.PROP_TITLE, new PropertyChangeListener() {
+              @Override
+              public void propertyChange(PropertyChangeEvent evt) {
+                setWindowTitle((String) evt.getNewValue());
+              }
+            });
+          }
+        }
+      }
+    });
+  }
+
+  private void setWindowTitle(final String title) {
+    for (IWorkbenchWindow w : PlatformUI.getWorkbench().getWorkbenchWindows()) {
+      final Shell s = w.getShell();
+      if (!s.isDisposed()) {
+        s.getDisplay().asyncExec(new Runnable() {
+          @Override
+          public void run() {
+            s.setText(title);
+          }
+        });
+      }
+    }
   }
 }
