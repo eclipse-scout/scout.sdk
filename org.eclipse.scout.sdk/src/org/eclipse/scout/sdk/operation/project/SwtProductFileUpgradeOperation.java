9a6a17615e84da08ee1eb535b97c1013a840a991
Judith Gull <jgu@bsiag.com>
2012-09-05 07:49:34 +0000

    https://bugs.eclipse.org/bugs/show_bug.cgi?id=387879
    Property clearPersistedState=true set to config.ini in e4 to avoid strange effects, if compontents do not exist anymore

--- org.eclipse.scout.sdk/src/org/eclipse/scout/sdk/operation/project/SwtProductFileUpgradeOperation.java
+++ org.eclipse.scout.sdk/src/org/eclipse/scout/sdk/operation/project/SwtProductFileUpgradeOperation.java
@@ -120,6 +120,8 @@ public class SwtProductFileUpgradeOperation extends AbstractScoutProjectNewOpera
         newEntry.append(",");
         newEntry.append(oldEntry.substring(pos));
         pfmh.ConfigurationFile.setOsgiBundlesEntry(newEntry.toString());
+        //clear saved user changes of the ui model before starting
+        pfmh.ConfigurationFile.setEntry("clearPersistedState", "true");
       }
       pfmh.save();
     }
