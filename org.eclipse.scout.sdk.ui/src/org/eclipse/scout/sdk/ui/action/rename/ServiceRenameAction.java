d1d29411bd485e242cf1d418a6f71fab3d220da8
Matthias Villiger <mvi@bsiag.com>
2013-03-26 11:35:56 +0100

    Renaming a Service does not rename the corresponding client proxy entry
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=404347

--- org.eclipse.scout.sdk.ui/src/org/eclipse/scout/sdk/ui/action/rename/ServiceRenameAction.java
+++ org.eclipse.scout.sdk.ui/src/org/eclipse/scout/sdk/ui/action/rename/ServiceRenameAction.java
@@ -10,13 +10,25 @@
  ******************************************************************************/
 package org.eclipse.scout.sdk.ui.action.rename;
 
+import java.util.HashMap;
+
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.core.IType;
+import org.eclipse.pde.core.plugin.IPluginElement;
+import org.eclipse.scout.sdk.extensions.runtime.classes.IRuntimeClasses;
+import org.eclipse.scout.sdk.jobs.OperationJob;
+import org.eclipse.scout.sdk.operation.IOperation;
 import org.eclipse.scout.sdk.ui.internal.ScoutSdkUi;
 import org.eclipse.scout.sdk.ui.internal.jdt.JdtRenameTransaction;
+import org.eclipse.scout.sdk.util.pde.PluginModelHelper;
 import org.eclipse.scout.sdk.util.type.TypeUtility;
+import org.eclipse.scout.sdk.util.typecache.IWorkingCopyManager;
+import org.eclipse.scout.sdk.workspace.IScoutBundle;
+import org.eclipse.scout.sdk.workspace.ScoutBundleFilters;
+import org.eclipse.scout.sdk.workspace.type.ScoutTypeUtility;
 
 public class ServiceRenameAction extends AbstractRenameAction {
 
@@ -53,7 +65,41 @@ public class ServiceRenameAction extends AbstractRenameAction {
   protected void fillTransaction(JdtRenameTransaction transaction, String newName) throws CoreException {
     transaction.add(m_serviceImplementation, newName);
     if (TypeUtility.exists(m_serviceInterface) && !m_serviceInterface.isBinary()) {
-      transaction.add(m_serviceInterface, "I" + newName);
+      final String newInterfaceName = "I" + newName;
+      final IScoutBundle interfaceBundle = ScoutTypeUtility.getScoutBundle(m_serviceInterface);
+      final String oldFqn = m_serviceInterface.getFullyQualifiedName();
+      final String newFqn = oldFqn.replace(m_serviceInterface.getElementName(), newInterfaceName);
+
+      transaction.add(m_serviceInterface, newInterfaceName);
+
+      // rename the client proxies manually because they are are not renamed by JDT above!
+      OperationJob j = new OperationJob(new IOperation() {
+        @Override
+        public void validate() throws IllegalArgumentException {
+        }
+
+        @Override
+        public void run(IProgressMonitor monitor, IWorkingCopyManager workingCopyManager) throws CoreException, IllegalArgumentException {
+          for (IScoutBundle clientBundle : interfaceBundle.getChildBundles(
+              ScoutBundleFilters.getMultiFilterAnd(ScoutBundleFilters.getWorkspaceBundlesFilter(), ScoutBundleFilters.getBundlesOfTypeFilter(IScoutBundle.TYPE_CLIENT)), true)) {
+            String attribName = "class";
+            PluginModelHelper h = new PluginModelHelper(clientBundle.getProject());
+            HashMap<String, String> attributes = new HashMap<String, String>(1);
+            attributes.put(attribName, oldFqn);
+            IPluginElement[] simpleExtensions = h.PluginXml.getSimpleExtensions(IRuntimeClasses.EXTENSION_POINT_CLIENT_SERVICE_PROXIES, IRuntimeClasses.EXTENSION_ELEMENT_CLIENT_SERVICE_PROXY, attributes);
+            for (IPluginElement proxy : simpleExtensions) {
+              proxy.setAttribute(attribName, newFqn);
+            }
+            h.save(); // only saves if there are changes
+          }
+        }
+
+        @Override
+        public String getOperationName() {
+          return "Rename client proxies";
+        }
+      });
+      j.schedule();
     }
   }
 
