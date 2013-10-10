addb4db1b3758a0474198a93bbaa09691019d18f
Matthias Villiger <mvi@bsiag.com>
2013-10-10 12:05:47 +0200

    Bug 417480 - SDK: Display all product files in the SDK
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=417480
    
    Also show all product files in the 'Add Scout Bundle' wizard.
    
    Change-Id: I34d68815a7eaa1301bd9e4d9c0f7ee8306eb84ca
    Reviewed-on: https://git.eclipse.org/r/17252
    Tested-by: Hudson CI
    Reviewed-by: Matthias Villiger <mvi@bsiag.com>
    IP-Clean: Matthias Villiger <mvi@bsiag.com>

--- org.eclipse.scout.sdk.ui/src/org/eclipse/scout/sdk/ui/internal/wizard/newbundle/ProductFileSelectionWizardPage.java
+++ org.eclipse.scout.sdk.ui/src/org/eclipse/scout/sdk/ui/internal/wizard/newbundle/ProductFileSelectionWizardPage.java
@@ -10,28 +10,22 @@
  ******************************************************************************/
 package org.eclipse.scout.sdk.ui.internal.wizard.newbundle;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
 import org.eclipse.core.resources.IFile;
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.scout.sdk.ScoutSdkCore;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.scout.sdk.Texts;
 import org.eclipse.scout.sdk.operation.project.add.ScoutProjectAddOperation;
 import org.eclipse.scout.sdk.ui.fields.bundletree.CheckableTree;
 import org.eclipse.scout.sdk.ui.fields.bundletree.ICheckStateListener;
 import org.eclipse.scout.sdk.ui.fields.bundletree.ITreeNode;
 import org.eclipse.scout.sdk.ui.fields.bundletree.NodeFilters;
-import org.eclipse.scout.sdk.ui.fields.bundletree.TreeNode;
 import org.eclipse.scout.sdk.ui.fields.bundletree.TreeUtility;
+import org.eclipse.scout.sdk.ui.internal.ScoutSdkUi;
 import org.eclipse.scout.sdk.ui.wizard.project.AbstractProjectNewWizardPage;
 import org.eclipse.scout.sdk.util.PropertyMap;
 import org.eclipse.scout.sdk.workspace.IScoutBundle;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.model.IWorkbenchAdapter;
 
 /**
  * <h3>{@link ProductFileSelectionWizardPage}</h3>
@@ -42,19 +36,14 @@ import org.eclipse.ui.model.IWorkbenchAdapter;
 public class ProductFileSelectionWizardPage extends AbstractProjectNewWizardPage {
 
   private final static String PROP_SELECTED_PRODUCTS = "selectedProductFilesToModify";
-
-  private final Set<IFile> m_productFiles;
-  private final List<ITreeNode> m_productNodes;
-
+  private final IScoutBundle m_scoutBundle;
   private CheckableTree m_bundleTree;
-  private ITreeNode m_invisibleRootNode;
 
-  public ProductFileSelectionWizardPage(Set<IFile> productFiles) {
+  public ProductFileSelectionWizardPage(IScoutBundle scoutBundle) {
     super(ProductFileSelectionWizardPage.class.getName());
     setTitle(Texts.get("SelectProductsToModify"));
     setDescription(Texts.get("ProductFileSelectionWizardMsg"));
-    m_productFiles = productFiles;
-    m_productNodes = new ArrayList<ITreeNode>();
+    m_scoutBundle = scoutBundle;
   }
 
   @Override
@@ -71,49 +60,31 @@ public class ProductFileSelectionWizardPage extends AbstractProjectNewWizardPage
 
   @Override
   protected void createContent(Composite parent) {
-    m_invisibleRootNode = buildProductTree();
+    setExcludePage(true);
 
-    m_bundleTree = new CheckableTree(parent, m_invisibleRootNode);
-    m_bundleTree.addCheckSelectionListener(new ICheckStateListener() {
-      @Override
-      public void fireNodeCheckStateChanged(ITreeNode node, boolean checkState) {
-        setProperty(PROP_SELECTED_PRODUCTS, m_bundleTree.getCheckedNodes());
-        pingStateChanging();
-      }
-    });
-    m_bundleTree.setChecked(m_productNodes.toArray(new ITreeNode[m_productNodes.size()]));
+    try {
+      ITreeNode root = TreeUtility.createProductTree(m_scoutBundle, null, true);
 
-    // layout
-    parent.setLayout(new GridLayout(1, true));
-    m_bundleTree.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL | GridData.GRAB_VERTICAL | GridData.FILL_BOTH));
-  }
+      ITreeNode[] productFileNodes = TreeUtility.findNodes(root, NodeFilters.getByType(TreeUtility.TYPE_PRODUCT_NODE));
 
-  private ITreeNode buildProductTree() {
-    m_productNodes.clear();
-    ITreeNode rootNode = new TreeNode(CheckableTree.TYPE_ROOT, "root");
-    rootNode.setCheckable(false);
-    rootNode.setVisible(false);
-    for (IFile prodFile : m_productFiles) {
-      IScoutBundle b = ScoutSdkCore.getScoutWorkspace().getBundleGraph().getBundle(prodFile.getProject());
-      if (b != null) {
-        ITreeNode bundleNode = TreeUtility.findNode(rootNode, NodeFilters.getByData(b));
-        if (bundleNode == null) {
-          bundleNode = TreeUtility.createBundleTreeNode(rootNode, b);
-          bundleNode.setCheckable(false);
+      m_bundleTree = new CheckableTree(parent, root);
+      m_bundleTree.addCheckSelectionListener(new ICheckStateListener() {
+        @Override
+        public void fireNodeCheckStateChanged(ITreeNode node, boolean checkState) {
+          setProperty(PROP_SELECTED_PRODUCTS, m_bundleTree.getCheckedNodes());
+          pingStateChanging();
         }
+      });
+      m_bundleTree.setChecked(productFileNodes);
 
-        IWorkbenchAdapter wbAdapter = (IWorkbenchAdapter) prodFile.getAdapter(IWorkbenchAdapter.class);
-        ImageDescriptor imageDescriptor = null;
-        if (wbAdapter != null) {
-          imageDescriptor = wbAdapter.getImageDescriptor(prodFile);
-        }
-        ITreeNode childNode = TreeUtility.createNode(bundleNode, TreeUtility.TYPE_PRODUCT_NODE, prodFile.getName(), imageDescriptor, 0, prodFile, false);
-        childNode.setCheckable(true);
-        bundleNode.addChild(childNode);
-        m_productNodes.add(childNode);
-      }
+      // layout
+      parent.setLayout(new GridLayout(1, true));
+      m_bundleTree.setLayoutData(new GridData(GridData.GRAB_HORIZONTAL | GridData.GRAB_VERTICAL | GridData.FILL_BOTH));
+
+      setExcludePage(productFileNodes.length < 1);
     }
-
-    return rootNode;
+    catch (CoreException e) {
+      ScoutSdkUi.logError("Unable to create product file tree", e);
+    }
   }
 }
