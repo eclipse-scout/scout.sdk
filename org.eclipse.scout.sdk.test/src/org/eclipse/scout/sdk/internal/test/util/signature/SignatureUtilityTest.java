1cbb78b2c414bbbd54311dc22ddabaa7d7c50648
Matthias Villiger <mvi@bsiag.com>
2014-05-26 11:31:43 +0200

    Performance tests disabled

--- org.eclipse.scout.sdk.test/src/org/eclipse/scout/sdk/internal/test/util/signature/SignatureUtilityTest.java
+++ org.eclipse.scout.sdk.test/src/org/eclipse/scout/sdk/internal/test/util/signature/SignatureUtilityTest.java
@@ -92,7 +92,8 @@ public class SignatureUtilityTest extends AbstractScoutSdkTest {
     Assert.assertTrue(imports.contains("java.util.List"));
   }
 
-  @Test(timeout = 250L)
+  @Test
+  //TODO: re-enable timeout as soon as a dedicated performance testing infrastructure is available (timeout = 250L)
   public void testClassParameterized() throws Exception {
     String signature = "[Ljava.lang.Class<+[Lcom.bsiag.scout.client.ui.desktop.outline.IOutline;>;";
     FullyQuallifiedValidator validator = new FullyQuallifiedValidator();
@@ -101,7 +102,8 @@ public class SignatureUtilityTest extends AbstractScoutSdkTest {
     Assert.assertEquals("java.lang.Class<? extends com.bsiag.scout.client.ui.desktop.outline.IOutline[]>[]", result);
   }
 
-  @Test(timeout = 8L)
+  @Test
+  //TODO: re-enable timeout as soon as a dedicated performance testing infrastructure is available (timeout = 8L)
   public void testComplexNestedArrayListHashMapArray() throws Exception {
     String signature = "[Ljava.util.HashMap<Ljava.util.ArrayList<[[Ljava.lang.String;>;Lorg.eclipse.scout.sdk.workspace.member.IScoutType;>;";
     FullyQuallifiedValidator validator = new FullyQuallifiedValidator();
