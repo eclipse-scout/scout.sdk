2bcc54e9ae4e76f580197db47d86783a750d39c8
Matthias Villiger <mvi@bsiag.com>
2015-01-14 16:14:45 +0100

    Product files may not be created in case of a new module. Not required to be available.

--- org.eclipse.scout.sdk/src/org/eclipse/scout/sdk/operation/project/ServerProductFileUpgradeOperation.java
+++ org.eclipse.scout.sdk/src/org/eclipse/scout/sdk/operation/project/ServerProductFileUpgradeOperation.java
@@ -11,11 +11,11 @@
 package org.eclipse.scout.sdk.operation.project;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.scout.commons.CollectionUtility;
 import org.eclipse.scout.sdk.compatibility.PlatformVersionUtility;
 import org.eclipse.scout.sdk.operation.util.JettyProductFileUpgradeOperation;
 import org.eclipse.scout.sdk.operation.util.OsgiSystemCapabilitiesAddOperation;
@@ -40,61 +40,63 @@ public class ServerProductFileUpgradeOperation extends AbstractScoutProjectNewOp
   }
 
   @Override
-  public void validate() {
-    super.validate();
-    if (m_serverDevProdFile == null || m_serverProdFile == null) {
-      throw new IllegalArgumentException("server products cannot be null.");
-    }
-  }
-
-  @Override
   public String getOperationName() {
     return "Upgrade Server Products";
   }
 
   @Override
   public void run(IProgressMonitor monitor, IWorkingCopyManager workingCopyManager) throws CoreException {
-    // Jetty update for DEV product
-    if (PlatformVersionUtility.isJunoOrLater(getTargetPlatformVersion())) {
-      JettyProductFileUpgradeOperation op = new JettyProductFileUpgradeOperation(new IFile[]{m_serverDevProdFile}); // only DEV product
-      op.validate();
-      op.run(monitor, workingCopyManager);
+    List<IFile> productFiles = new ArrayList<IFile>(2);
+
+    if (m_serverDevProdFile != null) {
+      // Jetty update for DEV product
+      if (PlatformVersionUtility.isJunoOrLater(getTargetPlatformVersion())) {
+        JettyProductFileUpgradeOperation op = new JettyProductFileUpgradeOperation(new IFile[]{m_serverDevProdFile}); // only DEV product
+        op.validate();
+        op.run(monitor, workingCopyManager);
+      }
+      productFiles.add(m_serverDevProdFile);
     }
 
-    // Servlet Plugin
-    String servletPlugin = null;
-    if (JdtUtility.isServlet31OrNewer()) {
-      servletPlugin = "org.eclipse.scout.rt.server.servlet31";
-    }
-    else {
-      servletPlugin = "org.eclipse.scout.rt.server.servlet25";
+    if (m_serverProdFile != null) {
+      productFiles.add(m_serverProdFile);
     }
 
-    // Java 1.8/1.6 fragment for JAX-WS
-    boolean isMin18 = isMinJavaVersion(1.8);
-    String jaxWsFragment = null;
-    if (isMin18) {
-      jaxWsFragment = "org.eclipse.scout.jaxws216.jre18.fragment";
-    }
-    else {
-      jaxWsFragment = "org.eclipse.scout.jaxws216.jre16.fragment";
-    }
+    if (!productFiles.isEmpty()) {
+      // Servlet Plugin
+      String servletPlugin = null;
+      if (JdtUtility.isServlet31OrNewer()) {
+        servletPlugin = "org.eclipse.scout.rt.server.servlet31";
+      }
+      else {
+        servletPlugin = "org.eclipse.scout.rt.server.servlet25";
+      }
 
-    // add jaxws fragments
-    ArrayList<IFile> productFiles = CollectionUtility.arrayList(m_serverDevProdFile, m_serverProdFile);
-    for (IFile f : productFiles) {
-      ProductFileModelHelper h = new ProductFileModelHelper(f);
-      h.ProductFile.addDependency(servletPlugin);
-      h.ProductFile.addDependency(jaxWsFragment);
+      // Java 1.8/1.6 fragment for JAX-WS
+      boolean isMin18 = isMinJavaVersion(1.8);
+      String jaxWsFragment = null;
+      if (isMin18) {
+        jaxWsFragment = "org.eclipse.scout.jaxws216.jre18.fragment";
+      }
+      else {
+        jaxWsFragment = "org.eclipse.scout.jaxws216.jre16.fragment";
+      }
 
-      h.save();
-    }
+      // add jaxws fragments
+      for (IFile f : productFiles) {
+        ProductFileModelHelper h = new ProductFileModelHelper(f);
+        h.ProductFile.addDependency(servletPlugin);
+        h.ProductFile.addDependency(jaxWsFragment);
 
-    if (isMin18 && !PlatformVersionUtility.isLunaOrLater(getTargetPlatformVersion())) {
-      String javaVersionStr = (String) getProperties().getProperty(PROP_JAVA_VERSION);
-      OsgiSystemCapabilitiesAddOperation osgiCapAddOperation = new OsgiSystemCapabilitiesAddOperation(productFiles, javaVersionStr);
-      osgiCapAddOperation.validate();
-      osgiCapAddOperation.run(monitor, workingCopyManager);
+        h.save();
+      }
+
+      if (isMin18 && !PlatformVersionUtility.isLunaOrLater(getTargetPlatformVersion())) {
+        String javaVersionStr = (String) getProperties().getProperty(PROP_JAVA_VERSION);
+        OsgiSystemCapabilitiesAddOperation osgiCapAddOperation = new OsgiSystemCapabilitiesAddOperation(productFiles, javaVersionStr);
+        osgiCapAddOperation.validate();
+        osgiCapAddOperation.run(monitor, workingCopyManager);
+      }
     }
   }
 }
