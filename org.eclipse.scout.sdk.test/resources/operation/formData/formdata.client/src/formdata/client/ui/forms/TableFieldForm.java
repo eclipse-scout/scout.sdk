52e3060b2812281a9395f8bd2f94d9679f03b9e7
Matthias Villiger <mvi@bsiag.com>
2015-02-11 13:03:55 +0100

    Bug 459496 - Missing columns in the generated TableRowData bean
    https://bugs.eclipse.org/bugs/show_bug.cgi?id=459496

--- org.eclipse.scout.sdk.test/resources/operation/formData/formdata.client/src/formdata/client/ui/forms/TableFieldForm.java
+++ org.eclipse.scout.sdk.test/resources/operation/formData/formdata.client/src/formdata/client/ui/forms/TableFieldForm.java
@@ -20,7 +20,9 @@ import org.eclipse.scout.rt.shared.TEXTS;
 
 import formdata.client.ui.forms.TableFieldForm.MainBox.CompanyTableField;
 import formdata.client.ui.forms.TableFieldForm.MainBox.PersonTableField;
+import formdata.client.ui.forms.TableFieldForm.MainBox.TableFieldWithExternalTableField;
 import formdata.client.ui.template.formfield.AbstractCompanyTableField;
+import formdata.client.ui.template.formfield.AbstractLoremTableField;
 import formdata.client.ui.template.formfield.AbstractTableWithExtKey;
 import formdata.shared.services.process.TableFieldFormData;
 
@@ -29,6 +31,10 @@ public class TableFieldForm extends AbstractForm {
 
   public TableFieldForm() throws ProcessingException {
     super();
+  }
+
+  public TableFieldWithExternalTableField getTableFieldWithExternalTableField() {
+    return getFieldByClass(TableFieldWithExternalTableField.class);
   }
 
   public CompanyTableField getCompanyField() {
@@ -120,5 +126,8 @@ public class TableFieldForm extends AbstractForm {
       }
     }
 
+    @Order(2000.0)
+    public class TableFieldWithExternalTableField extends AbstractLoremTableField {
+    }
   }
 }
