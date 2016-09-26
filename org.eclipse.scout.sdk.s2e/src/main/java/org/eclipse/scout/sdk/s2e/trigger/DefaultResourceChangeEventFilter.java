b3b84ac9770476f7302e9237c546b819d13c317d
Matthias Villiger <mvi@bsi-software.com>
2016-09-26 13:45:04 +0200

    Ignore debug events for derived resource update

--- org.eclipse.scout.sdk.s2e/src/main/java/org/eclipse/scout/sdk/s2e/trigger/DefaultResourceChangeEventFilter.java
+++ org.eclipse.scout.sdk.s2e/src/main/java/org/eclipse/scout/sdk/s2e/trigger/DefaultResourceChangeEventFilter.java
@@ -16,6 +16,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.function.Predicate;
 
+import org.apache.commons.lang3.Validate;
 import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.jobs.Job;
@@ -34,6 +35,7 @@ public class DefaultResourceChangeEventFilter implements Predicate<IResourceChan
   public static final String MAVEN_UPDATES = "org.eclipse.m2e.";
   public static final String SONAR_UPDATE = "org.sonarlint.";
   public static final String WEB_TOOLS_UPDATE = "org.eclipse.wst.";
+  public static final String DEBUG_EVENT = "org.eclipse.debug.";
   public static final String ANNOTATION_PROCESSING_JOB = "org.eclipse.core.internal.events.NotificationManager.NotifyJob";
   public static final String ANOTATION_PROCESSING_BUILD = "org.eclipse.core.internal.events.AutoBuildJob";
   public static final String EXTERNAL_FOLDER_UPDATE = "org.eclipse.jdt.internal.core.ExternalFoldersManager.RefreshJob";
@@ -47,7 +49,7 @@ public class DefaultResourceChangeEventFilter implements Predicate<IResourceChan
   private Collection<String> m_excludedJobNamePrefixes;
 
   public DefaultResourceChangeEventFilter() {
-    String[] exclusions = new String[]{GIT_UPDATES, MAVEN_UPDATES, SONAR_UPDATE, WEB_TOOLS_UPDATE,
+    String[] exclusions = new String[]{GIT_UPDATES, MAVEN_UPDATES, SONAR_UPDATE, WEB_TOOLS_UPDATE, DEBUG_EVENT,
         ANNOTATION_PROCESSING_JOB, ANOTATION_PROCESSING_BUILD, EXTERNAL_FOLDER_UPDATE, DEBUG_INIT, REFRESH_JOB,
         WORKSPACE_INIT_JOB, TEAM_UPDATES};
     m_excludedJobNamePrefixes = new ArrayList<>(exclusions.length);
@@ -55,6 +57,12 @@ public class DefaultResourceChangeEventFilter implements Predicate<IResourceChan
 
     m_isIgnoreBuildEvents = true;
     m_isIgnoreScoutSdkEvents = true;
+  }
+
+  public DefaultResourceChangeEventFilter(Collection<String> excludedJobNamePrefixes, boolean ignoreBuildEvents, boolean ignoreScoutSdkEvents) {
+    m_excludedJobNamePrefixes = new ArrayList<>(Validate.notNull(excludedJobNamePrefixes));
+    m_isIgnoreBuildEvents = ignoreBuildEvents;
+    m_isIgnoreScoutSdkEvents = ignoreScoutSdkEvents;
   }
 
   @Override
@@ -144,7 +152,7 @@ public class DefaultResourceChangeEventFilter implements Predicate<IResourceChan
    *          The new excluded job fully qualified class names. May be <code>null</code>.
    */
   public void setExcludedJobNamePrefixes(Collection<String> excludedJobNamePrefixes) {
-    if (excludedJobNamePrefixes == null) {
+    if (excludedJobNamePrefixes == null || excludedJobNamePrefixes.isEmpty()) {
       m_excludedJobNamePrefixes = Collections.emptyList();
     }
     else {
