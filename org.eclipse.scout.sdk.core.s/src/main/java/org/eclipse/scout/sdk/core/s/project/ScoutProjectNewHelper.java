d33d87672ad87c379b48e6c73182e769a05b904b
Arthur van Dorp <Arthur.vanDorp@bsi-software.com>
2018-02-20 15:43:24 +0100

    [release] version updated to 7.0.300.003_Oxygen_3_RC2

--- org.eclipse.scout.sdk.core.s/src/main/java/org/eclipse/scout/sdk/core/s/project/ScoutProjectNewHelper.java
+++ org.eclipse.scout.sdk.core.s/src/main/java/org/eclipse/scout/sdk/core/s/project/ScoutProjectNewHelper.java
@@ -47,7 +47,7 @@ import org.xml.sax.SAXException;
  */
 public final class ScoutProjectNewHelper {
 
-  public static final String SCOUT_ARCHETYPES_VERSION = "7.0.300-SNAPSHOT";
+  public static final String SCOUT_ARCHETYPES_VERSION = "7.0.300.003_Oxygen_3_RC2";
   public static final String SCOUT_ARCHETYPES_HELLOWORLD_ARTIFACT_ID = "scout-helloworld-app";
   public static final String SCOUT_ARCHETYPES_GROUP_ID = "org.eclipse.scout.archetypes";
 
