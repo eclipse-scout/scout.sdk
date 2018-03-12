901bdc67be35399256865da7f106070007b8e3b8
Matthias Villiger <mvi@bsi-software.com>
2018-03-12 16:01:57 +0100

    Java 10 preparation
    Add "var" as reserved keyword as defined in JEP 286: Local-Variable Type
    Inference

--- org.eclipse.scout.sdk.core/src/main/java/org/eclipse/scout/sdk/core/util/CoreUtils.java
+++ org.eclipse.scout.sdk.core/src/main/java/org/eclipse/scout/sdk/core/util/CoreUtils.java
@@ -494,7 +494,7 @@ public final class CoreUtils {
         if (javaKeyWords == null) {
           String[] keyWords = new String[]{"abstract", "assert", "boolean", "break", "byte", "case", "catch", "char", "class", "const", "continue", "default", "do", "double", "else", "enum", "extends", "final", "finally", "float", "for",
               "goto", "if", "implements", "import", "instanceof", "int", "interface", "long", "native", "new", "package", "private", "protected", "public", "return", "short", "static", "strictfp", "super", "switch", "synchronized", "this",
-              "throw", "throws", "transient", "try", "void", "volatile", "while", "false", "null", "true"};
+              "throw", "throws", "transient", "try", "void", "volatile", "while", "false", "null", "true", "var" /* java 10 */};
           Set<String> tmp = new HashSet<>(keyWords.length);
           for (String s : keyWords) {
             tmp.add(s);
