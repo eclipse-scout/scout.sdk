0c90ec18b7bed451fca8e7000db9f963971520db
Matthias Villiger <mvi@bsi-software.com>
2019-05-27 17:41:11 +0200

    Fix javadoc

--- org.eclipse.scout.sdk.s2e/src/main/java/org/eclipse/scout/sdk/s2e/environment/IWorkingCopyManager.java
+++ org.eclipse.scout.sdk.s2e/src/main/java/org/eclipse/scout/sdk/s2e/environment/IWorkingCopyManager.java
@@ -21,8 +21,8 @@ import org.eclipse.jdt.core.JavaModelException;
  * <p>
  * Holds working copies and releases or discards them based on error- and cancellation status of the task running.
  * <p>
- * A {@link IWorkingCopyManager} must only be used once. As soon as {@link #unregisterAll(boolean, IProgressMonitor)
- * unregisterAll} as been called, no more calls to {@link #register(ICompilationUnit, IProgressMonitor) register} or
+ * A {@link IWorkingCopyManager} must only be used once. As soon as it has been committed, no more calls to
+ * {@link #register(ICompilationUnit, IProgressMonitor) register} or
  * {@link #reconcile(ICompilationUnit, IProgressMonitor) reconcile} are allowed (transaction finished).
  *
  * @since 3.6.0 2014-05-16
@@ -40,11 +40,9 @@ public interface IWorkingCopyManager {
    *          progress should be reported
    * @return true if the given compilation unit was newly registered with this call, false if it was already registered
    * @throws JavaModelException
-   *           if the compilation unit could not become a working copy (e.g. if the given compilation unit is read
-   *           only).
+   *           if the compilation unit could not become a working copy (e.g. if the given compilation unit is read only).
    * @throws IllegalArgumentException
-   *           if {@link #unregisterAll(boolean, IProgressMonitor) unregisterAll} has already been called on this
-   *           instance (transaction finished).
+   *           if the transaction of this {@link IWorkingCopyManager} has already been committed.
    */
   boolean register(ICompilationUnit icu, IProgressMonitor monitor) throws JavaModelException;
 
@@ -55,14 +53,14 @@ public interface IWorkingCopyManager {
    * @param monitor
    *          A {@link IProgressMonitor} or {@code null} to check if the operation has been canceled and to report
    *          progress while saving
-   * @return {@code true} if the save was successful, {@code false} otherwise. The operation may be unsuccessful if it
-   *         has been canceled in the meanwhile or at least one resource cannot be written because it was read only.
+   * @return {@code true} if the save was successful, {@code false} otherwise. The operation may be unsuccessful if it has
+   *         been canceled in the meanwhile or at least one resource cannot be written because it was read only.
    */
   boolean checkpoint(IProgressMonitor monitor);
 
   /**
-   * When doing direct source changes on the compilation unit, a reconcile is required to fire element change deltas.
-   * This is not required if changes are done using for example
+   * When doing direct source changes on the compilation unit, a reconcile is required to fire element change deltas. This
+   * is not required if changes are done using for example
    * {@link IType#createMethod(String, IJavaElement, boolean, IProgressMonitor)} or
    * {@link IType#createField(String, IJavaElement, boolean, IProgressMonitor)}
    *
@@ -72,11 +70,10 @@ public interface IWorkingCopyManager {
    *          a progress monitor used to report progress while opening this compilation unit or {@code null} if no
    *          progress should be reported
    * @throws JavaModelException
-   *           if the contents of the original element cannot be accessed. Reasons include: The original Java element
-   *           does not exist or the given compilation unit is not registered in this manager.
+   *           if the contents of the original element cannot be accessed. Reasons include: The original Java element does
+   *           not exist or the given compilation unit is not registered in this manager.
    * @throws IllegalArgumentException
-   *           if {@link #unregisterAll(boolean, IProgressMonitor) unregisterAll} has already been called on this
-   *           instance (transaction finished).
+   *           if the transaction of this {@link IWorkingCopyManager} has already been committed.
    */
   void reconcile(ICompilationUnit icu, IProgressMonitor monitor) throws JavaModelException;
 
