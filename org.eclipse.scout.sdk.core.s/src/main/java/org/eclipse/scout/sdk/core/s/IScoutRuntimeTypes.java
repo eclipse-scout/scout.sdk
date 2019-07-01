b868db5dd198e7a8784338ea037f7fb783b7c4e7
Matthias Villiger <mvi@bsi-software.com>
2019-07-01 17:36:30 +0200

    Fix tests

--- org.eclipse.scout.sdk.core.s/src/main/java/org/eclipse/scout/sdk/core/s/IScoutRuntimeTypes.java
+++ org.eclipse.scout.sdk.core.s/src/main/java/org/eclipse/scout/sdk/core/s/IScoutRuntimeTypes.java
@@ -190,7 +190,6 @@ public interface IScoutRuntimeTypes {
   String IWizard = "org.eclipse.scout.rt.client.ui.wizard.IWizard";
   String IWizardStep = "org.eclipse.scout.rt.client.ui.wizard.IWizardStep";
   String IDataObject = "org.eclipse.scout.rt.dataobject.IDataObject";
-  String IPrettyPrintDataObjectMapper = "org.eclipse.scout.rt.dataobject.IPrettyPrintDataObjectMapper";
   String IUuId = "org.eclipse.scout.rt.dataobject.id.IUuId";
 
   // other runtime classes
