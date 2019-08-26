fa72e93b20459e9622d4d09aad443be664ba25eb
Matthias Villiger <mvi@bsi-software.com>
2019-08-26 11:25:38 +0200

    Handle exceptions in completion callbacks

--- org.eclipse.scout.sdk.s2i/src/main/kotlin/org/eclipse/scout/sdk/s2i/environment/OperationTask.kt
+++ org.eclipse.scout.sdk.s2i/src/main/kotlin/org/eclipse/scout/sdk/s2i/environment/OperationTask.kt
@@ -5,6 +5,7 @@ import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.progress.Task
 import com.intellij.openapi.project.Project
 import com.intellij.util.concurrency.AppExecutorUtil
+import org.eclipse.scout.sdk.core.log.SdkLog
 import org.eclipse.scout.sdk.core.s.environment.IFuture
 import org.eclipse.scout.sdk.core.util.CoreUtils
 import org.eclipse.scout.sdk.core.util.EventListenerList
@@ -17,6 +18,7 @@ open class OperationTask(title: String, project: Project, private val task: (Ide
 
     private val m_progress = FinalValue<ProgressIndicator>()
     private val m_listeners = EventListenerList()
+    private val m_name = CoreUtils.toStringIfOverwritten(task).orElse(null)
 
     override fun run(indicator: ProgressIndicator) {
         m_progress.setIfAbsent(indicator)
@@ -24,7 +26,7 @@ open class OperationTask(title: String, project: Project, private val task: (Ide
         val scoutProgress = indicator.toScoutProgress()
         val workForCommit = 10
         val workForTask = 1000
-        scoutProgress.init(CoreUtils.toStringIfOverwritten(task).orElse(null), workForTask + workForCommit)
+        scoutProgress.init(m_name, workForTask + workForCommit)
         runInTransaction(project, { scoutProgress.newChild(workForCommit) }) {
             task.invoke(scoutProgress.newChild(workForTask))
         }
@@ -53,19 +55,32 @@ open class OperationTask(title: String, project: Project, private val task: (Ide
 
     override fun onFinished() {
         super.onFinished()
-        m_listeners.get(OperationTaskListener::class.java)
-                .forEach { it.onFinished(this) }
+        try {
+            m_listeners.get(OperationTaskListener::class.java)
+                    .forEach { it.onFinished(this) }
+        } catch (e: RuntimeException) {
+            SdkLog.error("Error in {} when finishing task {}.", OperationTaskListener::class.java.simpleName, m_name, e)
+        }
     }
 
     override fun onThrowable(error: Throwable) {
         // no super call here
-        m_listeners.get(OperationTaskListener::class.java)
-                .forEach { it.onThrowable(this, error) }
+        try {
+            m_listeners.get(OperationTaskListener::class.java)
+                    .forEach { it.onThrowable(this, error) }
+        } catch (e: RuntimeException) {
+            SdkLog.error("Unable to complete task. Original error:", error)
+            SdkLog.error("Error in {} when finishing task {} exceptionally. See previous log for details about the original error of the task.", OperationTaskListener::class.java.simpleName, m_name, e)
+        }
     }
 
     override fun onCancel() {
         super.onCancel()
-        m_listeners.get(OperationTaskListener::class.java)
-                .forEach { it.onCancel(this) }
+        try {
+            m_listeners.get(OperationTaskListener::class.java)
+                    .forEach { it.onCancel(this) }
+        } catch (e: RuntimeException) {
+            SdkLog.error("Error in {} when cancelling task {}.", OperationTaskListener::class.java.simpleName, m_name, e)
+        }
     }
 }
