ef2cd9e90a496237d2991f7936639666c5fff33d
Matthias Villiger <mvi@bsi-software.com>
2019-10-14 11:34:02 +0200

    Add new Java 13 keyword

--- org.eclipse.scout.sdk.core/src/main/java/org/eclipse/scout/sdk/core/util/JavaTypes.java
+++ org.eclipse.scout.sdk.core/src/main/java/org/eclipse/scout/sdk/core/util/JavaTypes.java
@@ -197,7 +197,7 @@ public final class JavaTypes {
     return JAVA_KEYWORDS.computeIfAbsentAndGet(() -> {
       String[] keyWords = {"abstract", "assert", "boolean", "break", "byte", "case", "catch", "char", "class", "const", "continue", "default", "do", "double", "else", "enum", EXTENDS, "final", "finally", "float", "for",
           "goto", "if", "implements", "import", "instanceof", "int", "interface", "long", "native", "new", "package", "private", "protected", "public", "return", "short", "static", "strictfp", SUPER, "switch", "synchronized", "this",
-          "throw", "throws", "transient", "try", "void", "volatile", "while", "false", "null", "true"};
+          "throw", "throws", "transient", "try", "void", "volatile", "while", "false", "null", "true", "yield"};
       return Arrays.stream(keyWords).collect(collectingAndThen(toSet(), Collections::unmodifiableSet));
     });
   }
