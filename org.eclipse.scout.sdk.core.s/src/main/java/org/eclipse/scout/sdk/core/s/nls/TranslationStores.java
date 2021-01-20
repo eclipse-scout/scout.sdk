775af678f8ffa1c508e328cd4823f8b703285dc1
Matthias Villiger <mvi@bsi-software.com>
2021-01-20 13:12:44 +0100

    An empty translation store is no configuration error
    There might be empty text services prepared to be filled later on first
    use. This must not be a configuration error. Therefore adjust the log
    level to debug.

--- org.eclipse.scout.sdk.core.s/src/main/java/org/eclipse/scout/sdk/core/s/nls/TranslationStores.java
+++ org.eclipse.scout.sdk.core.s/src/main/java/org/eclipse/scout/sdk/core/s/nls/TranslationStores.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2020 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2021 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -371,7 +371,7 @@ public final class TranslationStores {
     }
 
     if (s.size() < 1) {
-      SdkLog.warning("{} does not contain any translations.", s);
+      SdkLog.debug("{} does not contain any translations.", s);
       return false;
     }
     return true;
