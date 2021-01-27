19b884f261cc9a5fc32c075a28eee225056e657f
Matthias Villiger <mvi@bsi-software.com>
2021-01-27 09:01:45 +0100

    Update plugin verifier IJ from 2020.3 to 2020.3.2 patch level

--- org.eclipse.scout.sdk.s2i/build.gradle.kts
+++ org.eclipse.scout.sdk.s2i/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2020 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2021 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -84,7 +84,7 @@ intellij {
     }
 
     tasks.withType<RunPluginVerifierTask> {
-        setIdeVersions(listOf("IU-2020.2.4", "IU-2020.3"))
+        setIdeVersions(listOf("IU-2020.2.4", "IU-2020.3.2"))
         setSubsystemsToCheck("without-android")
         failureLevel = FailureLevel.ALL
     }
