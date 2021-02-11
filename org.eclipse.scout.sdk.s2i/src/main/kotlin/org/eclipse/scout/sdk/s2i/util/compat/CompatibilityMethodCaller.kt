b2c04508cb828528515d8c54c39a320175e99752
Matthias Villiger <mvi@bsi-software.com>
2021-02-11 17:26:56 +0100

    CompatibilityMethodCaller: support null arguments

--- org.eclipse.scout.sdk.s2i/src/main/kotlin/org/eclipse/scout/sdk/s2i/util/compat/CompatibilityMethodCaller.kt
+++ org.eclipse.scout.sdk.s2i/src/main/kotlin/org/eclipse/scout/sdk/s2i/util/compat/CompatibilityMethodCaller.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2020 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2021 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -23,7 +23,7 @@ class CompatibilityMethodCaller<T> {
     }
 
     private val m_descriptors = ArrayList<MethodDescriptor<T>>()
-    private val m_selectedDescriptorWithCallable: Pair<MethodDescriptor<T>, (Any?, Array<out Any>) -> T> by lazy {
+    private val m_selectedDescriptorWithCallable: Pair<MethodDescriptor<T>, (Any?, Array<out Any?>) -> T> by lazy {
         m_descriptors.asSequence()
                 .mapNotNull { d -> d.resolvedCallable?.let { d to it } }
                 .firstOrNull() ?: throw SdkException("No compatible API function found.")
@@ -37,27 +37,27 @@ class CompatibilityMethodCaller<T> {
     fun withCandidate(declaringClassFqn: String, methodName: String, vararg parameterTypesFqn: String, handler: (ResolvedMethod<T>) -> T) =
             withCandidate(MethodDescriptor(declaringClassFqn, methodName, parameterTypesFqn, handler))
 
-    fun invoke(vararg arguments: Any): T {
+    fun invoke(vararg arguments: Any?): T {
         val (selectedDescriptor, methodCallable) = m_selectedDescriptorWithCallable
         return selectedDescriptor.handler.invoke(ResolvedMethod(selectedDescriptor, methodCallable, *arguments))
     }
 
-    class ResolvedMethod<R>(val descriptor: MethodDescriptor<R>, private val callable: (Any?, Array<out Any>) -> R, vararg val args: Any) {
+    class ResolvedMethod<R>(val descriptor: MethodDescriptor<R>, private val callable: (Any?, Array<out Any?>) -> R, vararg val args: Any?) {
 
-        fun invoke(obj: Any?, vararg parameters: Any) = try {
+        fun invoke(obj: Any?, vararg parameters: Any?) = try {
             callable.invoke(obj, parameters)
         } catch (e: InvocationTargetException) {
             throw e.targetException
         }
 
-        fun invokeStatic(vararg parameters: Any) = invoke(null, *parameters)
+        fun invokeStatic(vararg parameters: Any?) = invoke(null, *parameters)
     }
 
     class MethodDescriptor<R>(val declaringClassFqn: String, val methodName: String, val parameterTypesFqn: Array<out String>, val handler: (ResolvedMethod<R>) -> R) {
 
         private val m_resolvedClass = FinalValue<Class<*>>()
         private val m_resolvedParameterTypes = FinalValue<Array<out Class<*>>>()
-        internal val resolvedCallable: ((Any?, Array<out Any>) -> R)? by lazy { resolveCallable() }
+        internal val resolvedCallable: ((Any?, Array<out Any?>) -> R)? by lazy { resolveCallable() }
 
         constructor(declaringClass: Class<*>, methodName: String, parameterTypes: Array<out Class<*>>, handler: (ResolvedMethod<R>) -> R) : this(declaringClass.name, methodName, emptyArray(), handler) {
             m_resolvedClass.set(declaringClass)
@@ -65,7 +65,7 @@ class CompatibilityMethodCaller<T> {
         }
 
         @Suppress("UNCHECKED_CAST")
-        private fun resolveCallable(): ((Any?, Array<out Any>) -> R)? {
+        private fun resolveCallable(): ((Any?, Array<out Any?>) -> R)? {
             try {
                 val resolvedParameterTypes = resolvedParameterTypes()
                 val resolvedClass = resolvedClass()
