b2ea8c0c92c77c19c972186002d1544ef9654ac4
Matthias Villiger <mvi@bsi-software.com>
2021-03-14 09:49:07 +0100

    Scout type resolve must be executed in read action
    toScoutType uses containingModule which might require a read action.

--- org.eclipse.scout.sdk.s2i/src/main/kotlin/org/eclipse/scout/sdk/s2i/derived/DerivedResourceInputWithIdea.kt
+++ org.eclipse.scout.sdk.s2i/src/main/kotlin/org/eclipse/scout/sdk/s2i/derived/DerivedResourceInputWithIdea.kt
@@ -18,6 +18,7 @@ import org.eclipse.scout.sdk.core.model.api.IType
 import org.eclipse.scout.sdk.core.model.api.MissingTypeException
 import org.eclipse.scout.sdk.core.s.derived.IDerivedResourceInput
 import org.eclipse.scout.sdk.core.s.environment.IEnvironment
+import org.eclipse.scout.sdk.s2i.environment.IdeaEnvironment.Factory.computeInReadAction
 import org.eclipse.scout.sdk.s2i.resolvePsi
 import org.eclipse.scout.sdk.s2i.resolveSourceRoot
 import org.eclipse.scout.sdk.s2i.toIdea
@@ -27,13 +28,14 @@ import java.util.*
 
 open class DerivedResourceInputWithIdea(val type: PsiClass, private val name: String? = type.name) : IDerivedResourceInput {
 
-    override fun getSourceType(env: IEnvironment): Optional<IType> =
-            try {
-                Optional.ofNullable(type.toScoutType(env.toIdea(), false))
-            } catch (e: MissingTypeException) {
-                SdkLog.info("Unable to update DTO for '{}' because there are compile errors in the compilation unit.", toString(), e)
-                Optional.empty()
-            }
+    override fun getSourceType(env: IEnvironment): Optional<IType> = computeInReadAction(project(), true) {
+        try {
+            Optional.ofNullable(type.toScoutType(env.toIdea(), false))
+        } catch (e: MissingTypeException) {
+            SdkLog.info("Unable to update DTO for '{}' because there are compile errors in the compilation unit.", toString(), e)
+            Optional.empty()
+        }
+    }
 
     override fun getSourceFolderOf(t: IType, env: IEnvironment): Optional<IClasspathEntry> =
             Optional.ofNullable(t.resolvePsi()
