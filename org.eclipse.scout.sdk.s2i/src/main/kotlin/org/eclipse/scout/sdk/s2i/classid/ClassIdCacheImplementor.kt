4fe4758f376afe94a996b99864370b778882ebd0
Matthias Villiger <mvi@bsi-software.com>
2022-05-09 11:40:38 +0200

    ClassId validation: Skip psi events for PsiCompiledElements
    Compiled elements are not interesting as they cannot be changed/fixed
    anyway. Furthermore, JavaRecursiveElementWalkingVisitor should not be
    used for PsiCompiledElements. See PsiWalkingState.elementStarted for
    details.
    
    320598

--- org.eclipse.scout.sdk.s2i/src/main/kotlin/org/eclipse/scout/sdk/s2i/classid/ClassIdCacheImplementor.kt
+++ org.eclipse.scout.sdk.s2i/src/main/kotlin/org/eclipse/scout/sdk/s2i/classid/ClassIdCacheImplementor.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2021 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -195,7 +195,7 @@ class ClassIdCacheImplementor(val project: Project) : ClassIdCache {
     private inner class PsiListener : PsiTreeChangeAdapter() {
         override fun childrenChanged(event: PsiTreeChangeEvent) {
             val file = event.file ?: return
-            if (!file.language.isKindOf(JavaLanguage.INSTANCE) || !file.isValid || !file.isPhysical) {
+            if (!file.language.isKindOf(JavaLanguage.INSTANCE) || !file.isValid || !file.isPhysical || file is PsiCompiledElement) {
                 return
             }
             m_delayedProcessor.submit(file)
