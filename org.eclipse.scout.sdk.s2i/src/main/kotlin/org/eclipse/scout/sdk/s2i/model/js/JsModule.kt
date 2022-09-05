37f3b144d28e22a55e3c9d5642bff7edf37ed413
Matthias Villiger <mvi@bsi-software.com>
2022-09-05 13:29:33 +0200

    Prevent IOException when calling contentAsText on a directory

--- org.eclipse.scout.sdk.s2i/src/main/kotlin/org/eclipse/scout/sdk/s2i/model/js/JsModule.kt
+++ org.eclipse.scout.sdk.s2i/src/main/kotlin/org/eclipse/scout/sdk/s2i/model/js/JsModule.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010-2021 BSI Business Systems Integration AG.
+ * Copyright (c) 2010-2022 BSI Business Systems Integration AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -64,6 +64,7 @@ class JsModule(val name: String, val namespace: String, val sourceRoot: VirtualF
         fun parseNamespace(moduleMainFile: VirtualFile, pattern: Pattern): String? {
             val fileContent = moduleMainFile
                 .takeIf { it.isValid }
+                ?.takeIf { !it.isDirectory }
                 ?.takeIf { it.isInLocalFileSystem }
                 ?.contentAsText() ?: return null
             val matcher = pattern.matcher(fileContent)
