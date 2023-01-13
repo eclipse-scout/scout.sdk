62207cd2cec2ce90cac39b0d62f24fcb5895fd7b
Matthias Villiger <mvi@bsi-software.com>
2023-01-13 12:22:20 +0100

    ProjectFileIndex#getSourceFolder is deprecated in IJ 223

--- org.eclipse.scout.sdk.s2i/src/main/kotlin/org/eclipse/scout/sdk/s2i/Extensions.kt
+++ org.eclipse.scout.sdk.s2i/src/main/kotlin/org/eclipse/scout/sdk/s2i/Extensions.kt
@@ -24,8 +24,10 @@ import com.intellij.openapi.project.IndexNotReadyException
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.rootManager
 import com.intellij.openapi.projectRoots.JavaSdk
+import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.*
 import com.intellij.psi.search.GlobalSearchScope
@@ -105,8 +107,8 @@ fun IJavaEnvironment.toIdea() = unwrap() as JavaEnvironmentWithIdea
  * @return the module source root (source folder) or library source root in which this PsiElement exists.
  */
 fun PsiElement.resolveSourceRoot() = containingFile
-        ?.virtualFile
-        ?.let { ProjectFileIndex.getInstance(project).getSourceRootForFile(it) }
+    ?.virtualFile
+    ?.let { ProjectFileIndex.getInstance(project).getSourceRootForFile(it) }
 
 /**
  * Converts this [PsiClass] into its corresponding Scout [IType].
@@ -122,9 +124,9 @@ fun PsiElement.resolveSourceRoot() = containingFile
  * @return The [IType] corresponding to this [PsiClass].
  */
 fun PsiClass.toScoutType(env: IdeaEnvironment, returnReferencingModuleIfNotInFilesystem: Boolean = true): IType? =
-        containingModule(returnReferencingModuleIfNotInFilesystem)
-                ?.let { env.toScoutJavaEnvironment(it) }
-                ?.let { toScoutType(it) }
+    containingModule(returnReferencingModuleIfNotInFilesystem)
+        ?.let { env.toScoutJavaEnvironment(it) }
+        ?.let { toScoutType(it) }
 
 /**
  * @param env The [IJavaEnvironment] in which the type should be searched.
@@ -155,8 +157,8 @@ fun PsiElement.findEnclosingClass(typeFilterFqn: String, scoutApi: IScoutApi, ac
                 val iExtension = scoutApi.IExtension()
                 if (place.isInstanceOf(iExtension)) {
                     val extensionOwner = place.resolveTypeArgument(iExtension.ownerTypeParamIndex(), iExtension.fqn())
-                            ?.getCanonicalText(false)
-                            ?.let { module?.findTypeByName(it) }
+                        ?.getCanonicalText(false)
+                        ?.let { module?.findTypeByName(it) }
                     if (extensionOwner != null && extensionOwner.isInstanceOf(typeFilterFqn)) {
                         return extensionOwner
                     }
@@ -170,7 +172,7 @@ fun PsiElement.findEnclosingClass(typeFilterFqn: String, scoutApi: IScoutApi, ac
 
 /**
  * Gets the [PsiType] of the type argument which is declared by the [levelFqn] given at the [typeParamIndex] given.
- * @param typeParamIndex The zero based index of the type parameter as declared by the the class [levelFqn].
+ * @param typeParamIndex The zero based index of the type parameter as declared by the class [levelFqn].
  * @param levelFqn The fully qualified name of the class declaring the type parameter. This class must be a super type of this [PsiClass].
  * @return The [PsiType] of the type parameter specified in the context of this [PsiClass].
  */
@@ -199,9 +201,9 @@ fun PsiType.resolveTypeArgument(index: Int): PsiType? {
         return null
     }
     return resolveResult.substitutor.substitutionMap.entries
-            .filter { it.key.index == index }
-            .map { it.value }
-            .firstOrNull()
+        .filter { it.key.index == index }
+        .map { it.value }
+        .firstOrNull()
 }
 
 /**
@@ -223,8 +225,8 @@ fun PsiElement.containingModule(returnReferencingModuleIfNotInFilesystem: Boolea
         val searchElement = if (isInProject) this else psiFile ?: this
         return computeInReadAction(project) {
             this
-                    .takeIf { it.isValid }
-                    ?.let { ModuleUtilCore.findModuleForPsiElement(searchElement) }
+                .takeIf { it.isValid }
+                ?.let { ModuleUtilCore.findModuleForPsiElement(searchElement) }
         }
     } catch (e: IndexNotReadyException) {
         // in case the current call is already in a read-action.
@@ -246,11 +248,17 @@ fun PsiElement.requireScoutApi() = ApiHelper.requireScoutApiFor(this)
 fun Module.isJavaModule(): Boolean = rootManager.sdk?.sdkType == JavaSdk.getInstance()
 
 /**
+ * @return all source folders of this [Module].
+ */
+fun Module.sourceFolders() = ModuleRootManager.getInstance(this).contentEntries.asSequence()
+    .flatMap { it.sourceFolders.asSequence() }
+
+/**
  * @param scope The scope in which the PsiClasses should be searched.
  * @param checkDeep true if deep search should be performed.
- * @param includeAnonymous true if anonymous sub classes should be returned as well.
+ * @param includeAnonymous true if anonymous subclasses should be returned as well.
  * @param includeRoot true if the root [PsiClass] should be returned as well.
- * @return all sub types of this [PsiClass] that fulfill the given filter options.
+ * @return all subtypes of this [PsiClass] that fulfill the given filter options.
  */
 fun PsiClass.newSubTypeHierarchy(scope: SearchScope, checkDeep: Boolean, includeAnonymous: Boolean, includeRoot: Boolean): Query<PsiClass> {
     val children = ClassInheritorsSearch.search(this, scope, checkDeep, true, includeAnonymous)
@@ -267,7 +275,7 @@ fun PsiClass.newSubTypeHierarchy(scope: SearchScope, checkDeep: Boolean, include
  * @return The [VirtualFile] that corresponds to this [Path].
  */
 fun Path.toVirtualFile() = VfsUtil.findFile(this, true)
-        ?.takeIf { it.isValid }
+    ?.takeIf { it.isValid }
 
 /**
  * @param annotation The fully qualified name of the annotation the class must have.
@@ -289,11 +297,11 @@ fun Project.findAllTypesAnnotatedWith(annotation: String, scope: SearchScope, in
     options.addVariableConstraint(constraint)
 
     return structuralSearch(options, indicator)
-            .map { it.match }
-            .filter { it.isValid }
-            .filter { it.isPhysical }
-            .filter { it is PsiClass }
-            .map { it as PsiClass }
+        .map { it.match }
+        .filter { it.isValid }
+        .filter { it.isPhysical }
+        .filter { it is PsiClass }
+        .map { it as PsiClass }
 }
 
 /**
@@ -327,9 +335,9 @@ fun Project.findTypesByName(fqn: String) = findTypesByName(fqn, GlobalSearchScop
  * @return all PsiClasses within the given [GlobalSearchScope] having the given fully qualified name.
  */
 fun Project.findTypesByName(fqn: String, scope: GlobalSearchScope) =
-        computeInReadAction(this) { JavaPsiFacade.getInstance(this).findClasses(fqn, scope) }
-                .asSequence()
-                .filter { it.isValid }
+    computeInReadAction(this) { JavaPsiFacade.getInstance(this).findClasses(fqn, scope) }
+        .asSequence()
+        .filter { it.isValid }
 
 /**
  * Gets the [PsiClass] from the classpath of this [Module] having the give fully qualified name
@@ -338,13 +346,21 @@ fun Project.findTypesByName(fqn: String, scope: GlobalSearchScope) =
  * @return the [PsiClass] from the classpath of this [Module] having the give fully qualified name
  */
 fun Module.findTypeByName(fqn: String, includeTests: Boolean = true) = project
-        .findTypesByName(fqn, moduleWithDependenciesAndLibrariesScope(this, includeTests))
-        .firstOrNull()
+    .findTypesByName(fqn, moduleWithDependenciesAndLibrariesScope(this, includeTests))
+    .firstOrNull()
 
 /**
  * @return The [Module] within the given [Project] in which this file exists.
  */
 fun VirtualFile.containingModule(project: Project) = ModuleUtilCore.findModuleForFile(this, project)
+
+/**
+ * @return The source folder in which this file is stored. May return null in case the file is not part of a [Module] or is not below a source folder of the module.
+ */
+fun VirtualFile.containingSourceFolder(project: Project) = containingModule(project)?.sourceFolders()?.firstOrNull {
+    val sourceFolderDir = it.file
+    return@firstOrNull sourceFolderDir != null && VfsUtilCore.isAncestor(sourceFolderDir, this, false)
+}
 
 /**
  * @return The directory [Path] of this [Module]. It is the main content root of the [Module] or the directory containing the .iml file if no content roots are available
@@ -391,7 +407,7 @@ fun PsiClass.isInstanceOf(vararg parentFqn: String): Boolean = computeInReadActi
 /**
  * Tries to resolve the [PropertiesFile] that contains the text properties for the [Language] given.
  * @param language The [Language] of this [ITranslationStore] for which the [PropertiesFile] should be returned.
- * @param psiManager The [PsiManager] to lookup the file.
+ * @param psiManager The [PsiManager] to look up the file.
  * @return The [PropertiesFile] or null if no file could be found for the given [Language].
  */
 fun ITranslationStore.resolvePropertiesFile(language: Language, psiManager: PsiManager): PropertiesFile? {
@@ -417,13 +433,13 @@ fun IStackedTranslation.resolveProperty(language: Language, project: Project) = 
 /**
  * Tries to resolve the [com.intellij.lang.properties.IProperty] that corresponds to this [IStackedTranslation] and the [Language] given.
  * @param language The [Language] of this [IStackedTranslation] for which the [com.intellij.lang.properties.IProperty] should be returned.
- * @param psiManager The [PsiManager] to lookup the file.
+ * @param psiManager The [PsiManager] to look up the file.
  * @return The [com.intellij.lang.properties.IProperty] of this [IStackedTranslation] and the [Language] given or null if it could not be found.
  */
 fun IStackedTranslation.resolveProperty(language: Language, psiManager: PsiManager) = entry(language).orElse(null)
-        ?.store()
-        ?.resolvePropertiesFile(language, psiManager)
-        ?.findPropertyByKey(key())
+    ?.store()
+    ?.resolvePropertiesFile(language, psiManager)
+    ?.findPropertyByKey(key())
 
 /**
  * Tries to resolve the local [Path] of this [VirtualFile] if this [VirtualFile] points to such a file.
