dd2405e04abb7094681f6bcf3cddbfdc77ea268c
Matthias Villiger <mvi@bsi-software.com>
2024-03-13 14:34:02 +0100

    Compatibility for IJ 2024.1
    CodeFoldingConfigurable is internal in some IJ versions. Use constant as
    literal.

--- org.eclipse.scout.sdk.s2i/src/main/kotlin/org/eclipse/scout/sdk/s2i/settings/ScoutSearchableOptionContributor.kt
+++ org.eclipse.scout.sdk.s2i/src/main/kotlin/org/eclipse/scout/sdk/s2i/settings/ScoutSearchableOptionContributor.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2024 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -9,7 +9,6 @@
  */
 package org.eclipse.scout.sdk.s2i.settings
 
-import com.intellij.application.options.editor.CodeFoldingConfigurable
 import com.intellij.ide.ui.search.SearchableOptionContributor
 import com.intellij.ide.ui.search.SearchableOptionProcessor
 import com.intellij.openapi.application.ApplicationBundle
@@ -17,6 +16,7 @@ import com.intellij.openapi.application.ApplicationBundle
 class ScoutSearchableOptionContributor : SearchableOptionContributor() {
     override fun processOptions(processor: SearchableOptionProcessor) {
         // Add option for Scout code folding settings to be found when searching
-        processor.addOptions("scout texts", null, "scout texts", CodeFoldingConfigurable.ID, ApplicationBundle.message("group.code.folding"), false)
+        val id = "editor.preferences.folding" // from CodeFoldingConfigurable.ID which is internal in some IJ versions
+        processor.addOptions("scout texts", null, "scout texts", id, ApplicationBundle.message("group.code.folding"), false)
     }
 }
