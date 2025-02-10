028899511642f8d3f9a6cd4eb17c692b8fca842a
Matthias Villiger <mvi@bsi-software.com>
2025-02-10 13:08:07 +0100

    Remove Sonar configuration
    Sonar is not used since various releases

--- org.eclipse.scout.sdk.core.java/src/main/java/org/eclipse/scout/sdk/core/java/model/api/Flags.java
+++ org.eclipse.scout.sdk.core.java/src/main/java/org/eclipse/scout/sdk/core/java/model/api/Flags.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2025 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -175,7 +175,7 @@ public final class Flags {
    *          the flags
    * @return {@code true} if the element is marked as deprecated
    */
-  public static boolean isDeprecated(int flags) { //NOSONAR
+  public static boolean isDeprecated(int flags) {
     return (flags & AccDeprecated) != 0;
   }
 
