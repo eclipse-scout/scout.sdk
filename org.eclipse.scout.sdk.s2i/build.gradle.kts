b70a3b4fa787bdb7f12f775c604dccdeef05e305
Matthias Villiger <mvi@bsi-software.com>
2026-01-14 13:13:59 +0100

    Dependency Updates
    - Maven master
    - Eclipse Platform update to 2025-12
    - Maven Model update to 3.9.11
    - Logback update to 1.5.24
    - Mockito update to 5.21.0
    - Update javac compiler to 2.16.1
    - Gradle to 9.3.0
    - IntelliJ Gradle plugin to 2.10.5
    - poi-ooxml to 5.5.1
    - JUnit to 5.14.2

--- org.eclipse.scout.sdk.s2i/build.gradle.kts
+++ org.eclipse.scout.sdk.s2i/build.gradle.kts
@@ -29,7 +29,7 @@ plugins {
     id("maven-publish")
 
     // See https://github.com/JetBrains/intellij-platform-gradle-plugin
-    id("org.jetbrains.intellij.platform") version "2.10.1"
+    id("org.jetbrains.intellij.platform") version "2.10.5"
 
     kotlin("jvm") version "2.1.21"
     id("io.github.rmanibus.maven-settings") version "0.8" // for maven settings
@@ -49,8 +49,8 @@ repositories {
 dependencies {
     api("org.eclipse.scout.sdk:org.eclipse.scout.sdk.core.s:$scoutSdkVersion")
     api("org.eclipse.scout.sdk:org.eclipse.scout.sdk.core.java.ecj:$scoutSdkVersion")
-    implementation("org.apache.poi:poi-ooxml:5.4.1")
-    testImplementation("org.mockito:mockito-core:5.20.0")
+    implementation("org.apache.poi:poi-ooxml:5.5.1")
+    testImplementation("org.mockito:mockito-core:5.21.0")
     testImplementation("junit:junit:4.13.2")
     testRuntimeOnly("org.eclipse.scout.rt:org.eclipse.scout.rt.client:$scoutRtVersion") {
         // conflicts with the older version shipped with IJ. Exclude the Scout version to ensure the version is compatible with IJ.
@@ -98,11 +98,11 @@ intellijPlatform {
                 channels = listOf(ProductRelease.Channel.RELEASE)
                 sinceBuild = "2025.1"
             }
-            select {
-                types = listOf(IntelliJPlatformType.IntellijIdea)
-                channels = listOf(ProductRelease.Channel.EAP)
-                sinceBuild = "253"
-            }
+//            select {
+//                types = listOf(IntelliJPlatformType.IntellijIdea)
+//                channels = listOf(ProductRelease.Channel.EAP)
+//                sinceBuild = "253"
+//            }
         }
     }
 }
