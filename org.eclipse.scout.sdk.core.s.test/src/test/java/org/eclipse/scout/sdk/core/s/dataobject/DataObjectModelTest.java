101eb697427b2c0fe04df5070c2b18f11d71d1af
Matthias Villiger <mvi@bsi-software.com>
2026-08-28 14:39:55 +0200

    DoMethodGenerator should not create arguments with ? extends Object
    Fix Test
    
    419241

--- org.eclipse.scout.sdk.core.s.test/src/test/java/org/eclipse/scout/sdk/core/s/dataobject/DataObjectModelTest.java
+++ org.eclipse.scout.sdk.core.s.test/src/test/java/org/eclipse/scout/sdk/core/s/dataobject/DataObjectModelTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2023 BSI Business Systems Integration AG
+ * Copyright (c) 2010, 2026 BSI Business Systems Integration AG
  *
  * This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License 2.0
@@ -66,6 +66,6 @@ public class DataObjectModelTest {
         .map(DataObjectNode::name)
         .sorted()
         .collect(joining(","));
-    assertEquals("enabled,stringAttribute,versions", nodeNames);
+    assertEquals("enabled,keys,stringAttribute,versions", nodeNames);
   }
 }
